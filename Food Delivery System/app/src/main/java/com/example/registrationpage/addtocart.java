356b368347cbc687f13c12744ef948fd26f1e20c
Sreejan100 <chattopadhyaysreejan100@gmail.com>
2021-05-04 00:45:59 +0530

    "College Project"

--- Food Delivery System/app/src/main/java/com/example/registrationpage/addtocart.java
+++ Food Delivery System/app/src/main/java/com/example/registrationpage/addtocart.java
@@ -1,14 +1,48 @@
 package com.example.registrationpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.registrationpage.adapter.AddtoCartAdapter;
+import com.example.registrationpage.model.AddtoCart;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class addtocart extends AppCompatActivity {
+    RecyclerView addtocartrecycleview;
+    AddtoCartAdapter addtoCartAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtocart);
+
+        List<AddtoCart> listitems = new ArrayList<>();
+
+        listitems.add(new AddtoCart(R.drawable.popularfood1,"4.7","Float Cake Vietnam","$7.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood2,"4.2","Chicken Tikka","$6.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood3,"4.5","Panner Masala","$5.09"));
+        listitems.add(new AddtoCart(R.drawable.popularfood1,"4.7","Float Cake Vietnam","$7.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood2,"4.2","Chicken Tikka","$6.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood3,"4.5","Panner Masala","$5.09"));
+        listitems.add(new AddtoCart(R.drawable.popularfood1,"4.7","Float Cake Vietnam","$7.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood2,"4.2","Chicken Tikka","$6.05"));
+        listitems.add(new AddtoCart(R.drawable.popularfood3,"4.5","Panner Masala","$5.09"));
+
+        setAddtocart(listitems);
+
+    }
+
+    private void setAddtocart(List<AddtoCart> listitems) {
+
+        addtocartrecycleview = (RecyclerView)findViewById(R.id.cartrecyclerview);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
+        addtocartrecycleview.setLayoutManager(layoutManager);
+        addtoCartAdapter = new AddtoCartAdapter(this,listitems);
+        addtocartrecycleview.setAdapter(addtoCartAdapter);
     }
 }
